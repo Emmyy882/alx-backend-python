8297f8886033eb08db54d578b3d136489624c436
Emmyy882 <emmaagbaje3@gmail.com>
2024-03-14 20:21:07 +0100

    implemented type-annoted function

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""a type-annotated function that takes two float arguments and return..
+    their sum as a float
+"""
+def add(a: float, b: float) -> float:
+    return a + b
+
