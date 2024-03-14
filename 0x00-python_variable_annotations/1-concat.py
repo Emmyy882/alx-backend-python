cd2b1ada118342b0f1c81ee2fd75f67ebda6e92f
Emmyy882 <emmaagbaje3@gmail.com>
2024-03-14 20:38:17 +0100

    implemented type-annoted function

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+"""
+a script that concats two strings
+"""
+
+
+def concat(str1: str, str2: str) -> str:
+    """concat is a type-annotated function that takes two string as
+       arguments and returns a concatenated string
+    """
+    return str1 + str2
